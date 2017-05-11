f2435090fa6ec092a8bac9d3fa5a7a21dc25461b
Nam Truong <namtruongncn@gmail.com>
2017-05-11 14:02:19 +0700

    clean

--- header.php
+++ header.php
@@ -23,43 +23,6 @@
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'training' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-
-		<?php if ( is_front_page() ) : ?>
-		<h1 class="site-branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Twenty <span>by HTML5 UP</span></a>
-		</h1>
-		<?php else : ?>
-		<div class="site-branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Twenty <span>by HTML5 UP</span></a>
-		</div>
-		<?php endif; ?>
-
-		<nav id="site-navigation" class="main-navigation">
-			<?php wp_nav_menu( array(
-				'theme_location'  => 'primary',
-				'container_class' => 'menu-primary-container',
-				'container_id'    => 'primary-menu',
-				'menu_class' => 'menu clearfix',
-			) ); ?>
-		</nav>
-	</header><!-- #masthead -->
-
 	<div class="site-content">
-
-		<section id="banner">
-			<div class="inner">
-				<header><h2>TWENTY</h2></header>
-
-				<p>This is <strong>Twenty</strong>, a free<br>responsive template<br>by <a href="http://html5up.net">HTML5 UP</a>.</p>
-
-				<footer class="buttons_vertical">
-					<a href="#content" class="button fit scrolly">Tell me more</a>
-				</footer>
-			</div>
-		</section>
-
 		<div id="content" class="container sidebar-left">
 			<div class="row">
